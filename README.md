2920960f9d3910875e78c4d3f03d1f4e5b975f94
Catofwanders <baturataras@gmail.com>
2020-05-16 14:44:29 +0300

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+## Roadmap
+
+Create Web application with
+
+- webpack
+- react
+- redux
+- saga
+- firebase
+- jest
+- enzyme
+
+1 Create template
+2 Create User signIn/signUp
+3 Create Main Layout
+4 Create Users list page
+5 Create User settings page
+6 Create Posts page
---
+++ src/__tests__/fallback.spec.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+// import { render, fireEvent, waitForElement } from "@testing-library/react";
+import Fallback from "../components/common/fallback";
+
+describe("<Fallback />", () => {
+  test("should display a loader", async () => {});
+});
